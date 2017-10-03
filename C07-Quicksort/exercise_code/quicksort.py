0f1cd625b33b7142e0af6db2337f9fb47638e69c
Diego Saraiva <diegosaraiva@skynet.local>
2017-10-03 19:06:33 -0300

    correcting some exercises

--- C07-Quicksort/exercise_code/quicksort.py
+++ C07-Quicksort/exercise_code/quicksort.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-def quicksort(items, p, r):
-    if p < r:
-        q = partition(items, p, r)
-        quicksort(items, p, q-1)
-        quicksort(items, q+1, r)
 
 def partition(items, p, r):
     x = items[r]
@@ -23,8 +18,32 @@ def partition(items, p, r):
     items[i+1],items[r] = items[r],items[i+1]
     return i+1-count/2
 
+def modify_partition(A, p, r):
+    x = A[r]
+    i = p-1
+    l = p - 1
+
+    for j in range(p, r):
+        if A[j] < x:
+            i = i + 1
+            A[i],A[j] = A[j], A[i]
+            l = l + 1
+        elif A[j] == x:
+            l = l + 1
+            A[l],A[j] = A[j], A[l]
+    #
+    A[l+1],A[r] = A[r], A[l+1]
+    return ((i+l)/2) + 1
+
+def quicksort(A, p, r):
+    if p < r:
+        q = modify_partition(A, p, r)
+        assert(q == (p + r)/2)
+        quicksort(A, p, q-1)
+        quicksort(A, q+1, r)
 
 
 items = [2,5,9,3,7,0,-1]
-quicksort(items, 0, len(items)-1)
-print items
+A = [2,2,2,2,2,2,2,2]
+quicksort(A, 0, len(A)-1)
+print A
